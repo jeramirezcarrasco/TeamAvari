15d917da1f6ba5f07846ce18b1445d28f29c7db4
BlazeAlchemist <>
2019-06-01 13:18:10 -0400

    no message

---
+++ TeamAvariGameJam/Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform playerPos;
+    [SerializeField] private float speed;
+    [SerializeField] private float offset;
+
+    private void OnEnable()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+
+    private void Update()
+    {
+        if (playerPos != null && gameObject.activeSelf == true)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime); //you may need to change the vector2.____ part depending on which way the bullet is facing.
+        }
+    }
+
+}
---
+++ TeamAvariGameJam/Assets/Scripts/shooting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+
+    [SerializeField] private Transform shootPos;
+    ObjectPooler objectPooler;
+
+    [SerializeField] private float startTimeBtwShot;
+    [SerializeField] private float timeBtwShot;
+    public Transform Player;
+    public float speed;
+    public float attackspeed;
+
+
+    private void Awake()
+    {
+        objectPooler = ObjectPooler.Instance;
+    }
+
+    private void Start()
+    {
+        Player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        //if(timeBtwShot < 0.2)
+        //{
+        //    objectPooler.SpawnFromPool("Bullets", shootPos.position, transform.rotation);
+        //    timeBtwShot = startTimeBtwShot;
+        //}
+        //else
+        //{
+        //    timeBtwShot -= Time.deltaTime;
+        //}
+        Point();
+
+    }
+
+    private void Point()
+    {
+        Vector2 direction = Player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+
+}
